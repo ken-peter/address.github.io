cf61d727c1a1b7eec47f56de0892580666f8ff03
ken-peter <Ken Peter>
2017-02-27 15:11:23 +0300

    just befre first push

---
+++ README.md
@@ -0,0 +1,9 @@
+# ADDRESS BOOK
+##AUTHORS
+The project was created by Newton Kiragu and Kennedy Peter.
+
+##DESCRIPTION
+The project basically helps the user store contacts and contact details by prompting the user to insert them and storing them. The contacts are stored in an orderly manner and can be easily accessed upon demand by the user.
+
+##SETUP
+The folder can be accessed by downloading it and opening the main file using the default browser.
---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+// business  logic
+function Contact(first, last, number, email, location, country, notes) {
+  this.firstName = first;
+  this.lastName = last;
+  this.phoneNumber = number;
+  this.email = email;
+  this.location = location;
+  this.country = country;
+  this.notes = notes;
+
+}
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+// user interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var inputtedNumber = $("input#new-phone-number").val();
+    var inputtedEmail = $("input#new-email").val();
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedCountry = $("input#new-country").val();
+    var inputtedNotes = $("input#new-notes").val();
+
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedNumber, inputtedEmail, inputtedLocation, inputtedCountry, inputtedNotes);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-number").val("");
+    $("input#new-email").val("");
+    $("input#new-location").val("");
+    $("input#new-country").val("");
+    $("input#new-notes").val("");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $(".cont").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $(".number").text(newContact.number);
+      $(".email").text(newContact.email);
+      $(".location").text(newContact.location);
+      $(".country").text(newContact.country);
+      $(".notes").text(newContact.notes);
+    });
+  });
+});
